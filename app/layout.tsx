1665795157b560fd291b3f72bb103b2ec8281b83
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-09-07 23:43:30 +0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,16 @@ import { React } from 'react'
 import { AuthProvider } from '@/components/utils/provider/auth'
 const RootLayout = ({ children }: { children: React.Node }) => {
   return (
-    <AuthProvider>
+    <html>
+      <head>
+        <link rel='stylesheet' href = '/font/Aminute/Web-TT/Aminute.css'/>
+      </head>
+    <body>
+      <AuthProvider>
       { children }
     </AuthProvider>
+    </body>
+    </html>
   )
 }
 export default RootLayout;
