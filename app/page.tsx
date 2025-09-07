f3e07028d37a8012e642747d560612849d727886
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-09-08 00:03:53 +0600

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import Header from '@/components/header'
 export default function MainPage(){
   return (
-    <></>
+    <>
+      <main className = 'min-h-screen w-full'>
+        <Header context = {[]} user = {null}/>
+      </main>
+    </>
   )
 }
