4de84839f21e073f96d1c142534fb24ff6eafaea
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-09-09 22:34:06 +0600

    update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,13 @@
+import { Menu, Search, ArrowLeft} from 'lucide-react
+
 export default function LoginPage(){
   
   return (
     <>
       <main className = 'min-h-screen bg-gray-50'>
-        <div className =' sticky top-0 bg-white p-4 flex flex-row items-center justify-between'>
-          
+        <div className ='sticky top-0 bg-white p-4 flex flex-row items-center justify-between'>
+          <ArrowLeft className='w-4 h-4'/>
+          <h1>Authorization</h1>
         </div>
         <div className = 'h-full w-full flex flex-col items-center justify-center'>
           
