c1fe61ca4033b972179a54dbfec91bda0bc81d4e
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-09-09 23:00:32 +0600

    update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,9 +5,14 @@ export default function LoginPage(){
   return (
     <>
       <main className = 'min-h-screen bg-gray-50'>
-        <div className ='sticky top-0 bg-white p-4 flex flex-row items-center justify-between'>
+        <div className ='sticky top-0 bg-white p-4 flex flex-row items-center justify-between border-b'>
+          <div className = 'flex flex-row items-center gap-2'>
           <ArrowLeft className='w-4 h-4'/>
           <h1>Authorization</h1>
+          </div>
+          <div>
+            
+          </div>
         </div>
         <div className = 'h-full w-full flex flex-col items-center justify-center'>
           
