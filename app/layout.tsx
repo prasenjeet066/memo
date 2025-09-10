4e72e569ee55785f1e04185cce865c2605453a5d
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-09-10 18:45:21 +0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function LocaleLayout({ children }: Props) {
   return (
     <html>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Intel+One+Mono:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet"/>
         <link rel='stylesheet' href='/font/Aminute/Web-TT/Aminute.css'/>
       </head>
       <body>
