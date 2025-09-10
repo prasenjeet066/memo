377a167abbe2e6e09cce7b620a845e7438413b79
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-09-10 20:49:48 +0600

    update search.tsx

--- components/utils/search.tsx
+++ components/utils/search.tsx
@@ -4,8 +4,8 @@ const SearchInHeader = () => {
   return (
     <>
       <div className='flex flex-row items-center justify-between border bg-white  p-2 px-4 bg-gray-100 rounded-full '>
-        <input type='text' className='bg-none border-none text-sm outline-none'/>
-        <Search className='w-4 h-4  border-l text-gray-800'/>
+        <input type='text' className='bg-none border-none text-sm outline-none' placeholder='Search anything..'/>
+        <Search className='w-4 h-4 pl-2 border-l text-gray-800'/>
       </div>
     </>
   )
