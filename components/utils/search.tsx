16806110983e904775f620835ba96fd8edc1eae5
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-09-10 19:37:12 +0600

    update search.tsx

--- components/utils/search.tsx
+++ components/utils/search.tsx
@@ -3,9 +3,9 @@ import { Menu, Search, ArrowLeft} from 'lucide-react'
 const SearchInHeader = () => {
   return (
     <>
-      <div className='flex flex-row items-center justify-between border bg-white p-2 border-gray-100 rounded-xl'>
+      <div className='flex flex-row items-center justify-between border bg-white px-2 border-gray-100 rounded-xl'>
         <input type='text' className='bg-none border-none outline-none'/>
-        <Search className='w-4 h-4 p-2 border-l'/>
+        <Search className='w-4 h-4 p-2 border-l text-gray-800'/>
       </div>
     </>
   )
