2c1be4c1ec3ed75192ec1bc531c9a81e314f88ea
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-09-11 20:39:24 +0600

    update provider.ts

--- lib/security/auth/db/provider.ts
+++ lib/security/auth/db/provider.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = 'mongodb+srv://howladerprasenjeet440:6ehMc6iIeh2SDCvJ@cluster0.joo9psn.mongodb.net/codes_beta'
 
 interface MongooseCache {
   conn: typeof mongoose | null
