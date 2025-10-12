91f6c7d1972fbb7e7e5fb0722ed6d6d120d27a1e
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-10-12 21:31:12 +0600

    update EditorToolbar.tsx

--- components/record/EditorToolbar.tsx
+++ components/record/EditorToolbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Fai } from '@/components/Fontawesome'
 import { toolbarBlocks } from "@/lib/editor/toolbarConfig";
 import {
