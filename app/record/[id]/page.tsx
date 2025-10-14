0e225956b0aaae99f7e753a8131d8977559eada7
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-10-14 12:45:18 +0600

    update page.tsx

--- app/record/[id]/page.tsx
+++ app/record/[id]/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useState, useEffect, useCallback, useRef } from 'react';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Fai } from '@/components/Fontawesome';
