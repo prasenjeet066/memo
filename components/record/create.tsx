006c9f30f4502eb25fe35ff349937728022b54bb
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-10-18 10:03:09 +0600

    update

--- components/record/create.tsx
+++ components/record/create.tsx
@@ -287,6 +287,7 @@ export default function CreateNew({
           break;
           
         case 'table': {
+          alert('tble')
           const tableId = `table-${Date.now()}`;
           const makeHeader = (j: number) => `<th contenteditable>Header ${j + 1}</th>`;
           const makeCell = (i: number, j: number) => `<td contenteditable>Row ${i + 1}, Col ${j + 1}</td>`;
