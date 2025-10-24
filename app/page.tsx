d1199351ce8b38723863a1a8599ebd503c03db19
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-10-24 12:01:00 +0600

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -107,7 +107,7 @@ export default function MainPage() {
         <div class = 'flex flex-col w-full h-full items-center justify-center'>
         <div className='flex flex-col items-center justify-center h-full'>
           
-        <Image placeholder='blur' alt= 'null' className = 'w-full' src = {`https://memoorg.vercel.app/api/img/vor?url=https://www.kindpng.com/picc/m/490-4906364_latest-traditional-indian-jewellery-hd-png-download.png`} height={800} width={800}/>
+        <Image placeholder='blur' alt= 'null' className = 'w-full' src = {`https://memoorg.vercel.app/api/img/vor`} height={800} width={800}/>
         <div className="p-4 space-y-2 w-full max-w-md">
           <h1 className="text-center text-2xl mt-4 font-semibold">
             Find Anything.
