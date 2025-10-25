3b7665ac7dac641c3d4bd938628f4e77df77d4fa
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-10-25 12:47:33 +0600

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -114,7 +114,7 @@ export default function MainPage() {
           <h1 className="text-center text-2xl mt-4 font-semibold">
             Find Anything.
           </h1>
-          <StarBorder as="div" className={`w-full flex items-center gap-2 rounded-full p-2 ${!isMobile ?'bg-white/50 backdrop:blur-md' : 'bg-white'}`} color="black" speed="5s">
+          <div className={`w-full  flex items-center gap-2 rounded-full p-2 ${!isMobile ?'bg-white/50 backdrop:blur-md' : 'bg-white'}`} color="black" speed="5s">
       
             <input
               type="text"
@@ -128,7 +128,7 @@ export default function MainPage() {
             >
               <Fai icon="arrow-right" />
             </button>
-            </StarBorder>
+            </div>
           
           
         </div>
