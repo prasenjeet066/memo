dbfbba562d3f642b1c443a1ef084bb7ec8a833e5
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-10-27 21:44:58 +0600

    update route.ts

--- app/api/img/vor/route.ts
+++ app/api/img/vor/route.ts
@@ -61,7 +61,7 @@ export async function GET(request) {
       ctx.closePath();
       
       ctx.strokeStyle =
-        brightness < 1 ? `rgba(${r},${g},${b},0.9)` : `rgba(255,255,255,0.3)`;
+        brightness < 1 ? `rgba(${r},${g},${b},0.9)` : `rgba(255,255,255,0.0)`;
       ctx.lineWidth = 1;
       ctx.stroke();
     }
