863089b9c11e2749610522cea19770b4bd8bc53a
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-10-27 21:37:00 +0600

    update

--- components/record/createx.tsx
+++ components/record/createx.tsx
@@ -1154,7 +1154,7 @@ export default function EnhancedEditor({
   return (
     <div className="w-full h-full flex flex-col">
       
-      <div className={"sm:px-2 px-4 py-3 flex items-center justify-between"}>
+      <div className={"px-4 py-3 flex items-center justify-between"}>
         <div className="flex items-center gap-4">
           <h1 className="text-xl font-bold text-gray-900">
           { record_name || 'Untitled Document'}
