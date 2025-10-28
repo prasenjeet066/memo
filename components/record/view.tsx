c688c7f68fa367faaf9783ec40e31b098ad72138
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-10-28 19:07:46 +0600

    update view.tsx

--- components/record/view.tsx
+++ components/record/view.tsx
@@ -150,12 +150,12 @@ export const Viewer = function({ __data }: Props) {
   
   return (<div className="w-full h-full flex flex-col">
   {/* Header / Title */}
-  <div className="px-4 py-3 flex items-center justify-between">
+  <div className="px-4 w-full py-3 flex items-center justify-between">
     <div className="flex items-center gap-4">
       <h1 className="font-bold text-gray-900 text-lg sm:text-xl md:text-2xl">
         {data.title || 'Untitled Article'}
       </h1>
-      <div className="flex items-center border-l pl-2">
+      <div className="flex items-center justify-end border-l pl-2">
       {isEditableForMe ? (
       
         <button
@@ -183,9 +183,9 @@ export const Viewer = function({ __data }: Props) {
   </div>
 
   {/* Action Buttons */}
-  <div className = 'flex px-4  items-center justify-start'>
-    <button onClick={()=>setActivePaper((prev)=> prev!=='overview' ? 'overview': prev)} className={'bg-none p-2 px-3 font-semibold ' + activePaper === 'overview' ? 'text-gray-900':'text-gray-500'}>{'Overview'}</button>
-    <button onClick={()=>setActivePaper((prev)=> prev!=='discussion' ? 'discussion' : prev)} className={'bg-none p-2 px-3 font-semibold ' + activePaper === 'discussion' ? 'text-gray-900' : 'text-gray-500'}>{'Discussion'}</button>
+  <div className = 'flex px-4 border-b w-full mb-2 items-center gap-4 justify-start'>
+    <button onClick={()=>setActivePaper((prev)=> prev!=='overview' ? 'overview': prev)} className={'bg-none p-2 px-3 font-semibold ' + activePaper === 'overview' ? 'text-gray-900 border-b border-gray-900':'text-gray-500'}>{'Overview'}</button>
+    <button onClick={()=>setActivePaper((prev)=> prev!=='discussion' ? 'discussion' : prev)} className={'bg-none p-2 px-3 font-semibold ' + activePaper === 'discussion' ? 'text-gray-900 border-b border-gray-900' : 'text-gray-500'}>{'Discussion'}</button>
   </div>
   {/* Article Content */}
   <div className="flex items-start justify-between">
